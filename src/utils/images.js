adf6cac32fa0646cd8a931c62a4e5ff5423d4149
Katemp23 <kmunoz14@soy.sena.edu.co>
2024-05-23 20:56:32 -0500

    Versión inicial del proyecto con dummyjson

---
+++ src/utils/images.js
@@ -0,0 +1,11 @@
+import slider_img_1 from "../assets/images/slider_img_1.jpg";
+import slider_img_2 from "../assets/images/slider_img_2.jpg";
+import shopping_cart from "../assets/images/shopping_cart.png";
+import correct from "../assets/images/correct.png";
+import loader from "../assets/images/loader.svg";
+import sliderImg1 from "../assets/images/1.png";
+import sliderImg2 from "../assets/images/2.png";
+import sliderImg3 from "../assets/images/3.png";
+ 
+const sliderImgs = [slider_img_1, slider_img_2, sliderImg1, sliderImg2, sliderImg3];
+export {sliderImgs, shopping_cart, correct, loader};
